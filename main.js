884878d41ea64ce054c2ec44354feb7324be9acd
beonezero <97246653+beonezero@users.noreply.github.com>
2023-04-20 03:49:22 +0300

    Update main.js

--- main.js
+++ main.js
@@ -3,3 +3,7 @@ const autoBrands = ["Audi", "BMW", "Tesla", "Nissan", "Peugeot"];
 for (i = 0; i < autoBrands.length; i++) {
   console.log(autoBrands[i]);
 }
+
+for (let brand of autoBrands) {
+  console.log(brand);
+}
