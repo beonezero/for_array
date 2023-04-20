cf8c8176aee86c36483d20f22b04126b5d0df669
beonezero <97246653+beonezero@users.noreply.github.com>
2023-04-20 04:05:27 +0300

    Update main.js

--- main.js
+++ main.js
@@ -7,3 +7,13 @@ for (i = 0; i < autoBrands.length; i++) {
 for (let brand of autoBrands) {
   console.log(brand);
 }
+autoBrands.forEach(function (brand, i) {
+  console.log(`${brand} => ${i}`);
+});
+autoBrands.forEach(printAutoBrand);
+
+function printAutoBrand(brand, i) {
+  console.log(`${brand} => ${i}`);
+}
+
+autoBrands.forEach((brand, i) => console.log(`${brand} => ${i}`));
